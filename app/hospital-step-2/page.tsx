76f628d00694633776cfe454aea9d92b21ea6347
WhatsWrongOB <whatswrongobaid@gmail.com>
2025-03-05 18:47:26 +0000

    -make changes

--- app/hospital-step-2/page.tsx
+++ app/hospital-step-2/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { Card, CardContent } from "@/components/ui/card";
 import cafe from "../../public/cafe.svg";
